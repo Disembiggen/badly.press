c5e1ec21866a02ea53a07b006af394544ce5f386
jane <janeaditchfield@gmail.com>
2026-09-10 19:36:13 +0100

    inital

---
+++ js/script.js
@@ -0,0 +1,35 @@
+console.log('script loaded');
+
+
+//mobile menu toggle
+const mobilemenu = document.getElementById('m-btn');
+mobilemenu.addEventListener('click', function() {
+    console.log('togglemenu');
+    document.getElementById('menu-list').classList.toggle("show");
+});
+
+
+const comments = document.querySelectorAll('.comment');
+if ( comments.length > 0 ){
+    //update comment count
+    document.getElementById('comment-count').innerText = comments.length;
+}
+
+
+const dither_icons = document.querySelectorAll('.dither-toggle');
+dither_icons.forEach(icon => {
+	icon.addEventListener('click', function() {
+	    let figure = icon.closest('.figure-controls').previousElementSibling;
+	    let img = figure.querySelector('img');
+
+	    if( figure.getAttribute('data-imgstate') == "dither"){
+	    	figure.setAttribute('data-imgstate', 'undither');	    	
+	    	let original = img.getAttribute('data-original');
+	    	img.src = original;
+	    }else{
+	    	figure.setAttribute('data-imgstate', 'dither');
+	    	let dither= img.getAttribute('data-dither');
+	    	img.src = dither;
+	    }    
+	});
+});
